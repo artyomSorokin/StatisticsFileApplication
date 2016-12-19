78c8a9306608acdd7dedd1f16d819c162b6a1748
artyomSorokin <artyom_sorokin@mail.ru>
2016-12-19 12:10:33 +0200

    Update HandleTextFromUserController.java

--- src/main/java/sorokin/web/HandleTextFromUserController.java
+++ src/main/java/sorokin/web/HandleTextFromUserController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import sorokin.Service.StatisticsTextFromUserService;
+import sorokin.service.StatisticsTextFromUserService;
 import sorokin.entity.StatisticsFileEntity;
 
 import java.util.List;
